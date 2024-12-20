0b93617d2d7bb4fc5c476dffbf107b5542195ebf
Mohit yadav <y2641699@gmail.com>
2024-12-20 10:09:53 +0530

    fibonacci number

---
+++ DP-fibonacci number.py
@@ -0,0 +1,15 @@
+# fibonacci number in dynamic programming(LEETCODE PROBLEM)
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {0:0, 1:1}
+
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = f(x-1) + f(x-2)
+                return memo[x]
+        return f(n)
+
+        # time comlexity O(n)
+        # space complexity O(n)
